24ef4da435171eb030370b4e6bcd2bca281cc57b
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-24 10:50:37 +0530

    hello appt

---
+++ Even.java
@@ -0,0 +1,20 @@
+class Even{
+	public static void main (String[] args)
+{
+
+
+	byte evenNum = 8;
+		if(evenNum %2==0)
+{
+System.out.println("its odd number:-");
+}
+
+else
+
+{
+System.out.println("its even number:-");
+}
+
+}
+
+}

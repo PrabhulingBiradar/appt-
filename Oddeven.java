eeb5608ee0132bca16ea269a3121dfdf4b2e4088
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-24 10:51:22 +0530

    hello appt oddeven

---
+++ Oddeven.java
@@ -0,0 +1,34 @@
+class Oddeven{
+
+	public static void main(String[] args)
+{
+
+for(int number = 1;number <40;number++)
+{
+if(number % 2 == 0)
+{
+System.out.println(number+ " is even number");
+}
+
+ else
+
+{
+	System.out.println(number+" is odd number");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
+}
